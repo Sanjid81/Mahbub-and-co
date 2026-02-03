6df0870bcf178416647d6ede4adf8716cf50e037
Sanjida Akter <youremail@example.com>
2026-02-03 12:13:21 +0600

    add contact us and recognition page

--- components/contact-us/contact-form.php
+++ components/contact-us/contact-form.php
@@ -1,62 +1,67 @@
 <div class="contact-us-section">
     <div class="contact-container">
-        <div class="contact-header">
-            <h1>
-                <?php echo esc_html($fields['contact_title']); ?>
-            </h1>
-            <p>
-                <?php echo esc_html($fields['contact_description']); ?>
-            </p>
-        </div>
+
 
         <div class="contact-content">
 
             <!-- Left Side -->
-            <div class="contact-left">
-                <div class="office-address-content">
-
-                    <h3>Address</h3>
-
-                    <?php if (!empty($fields['office_addresses'])): ?>
-                        <?php foreach ($fields['office_addresses'] as $office): ?>
-                            <div class="office-section">
-                                <h4>
-                                    <?php echo esc_html($office['office_title']); ?>
-                                </h4>
-                                <?php
-                                $lines = explode("\n", $office['office_address']);
-                                foreach ($lines as $line) {
-                                    echo '<p>' . esc_html($line) . '</p>';
-                                }
-                                ?>
-                            </div>
-                        <?php endforeach; ?>
-                    <?php endif; ?>
+            <div>
+                <div class="contact-header">
+                    <h1 class="heading-one">
+                        <?php echo esc_html($fields['contact_title']); ?>
+                    </h1>
+                    <p class="body-text">
+                            <?php echo esc_html($fields['contact_description']); ?>
+                    </p>
                 </div>
+                <div class="contact-left">
 
-                <div class="office-cotact-info">
-                    <?php if (!empty($fields['contact_email'])): ?>
-                        <div class="email-section">
-                            <h3>Email Us</h3>
-                            <a href="mailto:<?php echo esc_attr($fields['contact_email']); ?>">
-                                <?php echo esc_html($fields['contact_email']); ?>
-                            </a>
-                        </div>
-                    <?php endif; ?>
+                    <div class="office-address-content">
 
-                    <div class="office-section">
-                        <h3>
-                            <?php echo esc_html($fields['office_hours_title']); ?>
-                        </h3>
-                        <p>
-                            <?php echo esc_html($fields['office_time']); ?>
-                        </p>
-                        <p>
-                            <?php echo esc_html($fields['office_days']); ?>
-                        </p>
+                        <h3 class="heading-three">Address</h3>
+
+                        <?php if (!empty($fields['office_addresses'])): ?>
+                            <?php foreach ($fields['office_addresses'] as $office): ?>
+                                <div class="office-section">
+                                    <h3 class="heading-three">
+                                        <?php echo esc_html($office['office_title']); ?>
+                                    </h3>
+                                    <?php
+                                    $lines = explode("\n", $office['office_address']);
+                                    foreach ($lines as $line) {
+                                        echo '<p>' . esc_html($line) . '</p>';
+                                    }
+                                    ?>
+                                </div>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
                     </div>
-                </div>
 
+                    <div class="office-cotact-info">
+                        <?php if (!empty($fields['contact_email'])): ?>
+                            <div class="email-section">
+                                <h3 class="heading-three">
+                                    Email Us</h3>
+                                <a href="mailto:<?php echo esc_attr($fields['contact_email']); ?>" class="body-text">
+                                    <?php echo esc_html($fields['contact_email']); ?>
+                                </a>
+                            </div>
+                        <?php endif; ?>
+
+                        <div class="office-section">
+                            <h3 class="heading-three">
+                                <?php echo esc_html($fields['office_hours_title']); ?>
+                            </h3>
+                            <p>
+                                <?php echo esc_html($fields['office_time']); ?>
+                            </p>
+                            <p>
+                                <?php echo esc_html($fields['office_days']); ?>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
             </div>
 
             <!-- Right Side -->
